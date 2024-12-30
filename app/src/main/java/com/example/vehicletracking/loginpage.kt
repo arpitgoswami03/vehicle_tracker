b6f1df98d9b9722bf8936a557cd29bf782bbfb14
arpitgoswami03 <arpit.personal.2003@gmail.com>
2024-12-30 18:29:30 +0530

    added sharedPreference to store the username to use for DB purpose

--- app/src/main/java/com/example/vehicletracking/loginpage.kt
+++ app/src/main/java/com/example/vehicletracking/loginpage.kt
@@ -1,7 +1,11 @@
 package com.example.vehicletracking
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.provider.Settings.Global.putString
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +20,7 @@ import com.google.firebase.database.ValueEventListener
 
 class loginpage : AppCompatActivity() {
     private lateinit var binding: ActivityLoginpageBinding
+    private lateinit var sharedPreferences: SharedPreferences
     private val database = FirebaseDatabase.getInstance()
     private val reference = database.getReference("Users")
 
@@ -23,6 +28,8 @@ class loginpage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginpageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
         binding.signupGo.setOnClickListener {
             val intent = Intent(this, signuppage::class.java)
             startActivity(intent)
@@ -30,38 +37,45 @@ class loginpage : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             val usernameT = binding.username.text.toString()
             val passwordT = binding.password.text.toString()
-            if(usernameT.isNotEmpty() && passwordT.isNotEmpty()){
+            if (usernameT.isNotEmpty() && passwordT.isNotEmpty()) {
+                Log.i("firebase", "All fields are filled")
                 reference.child(usernameT).get().addOnSuccessListener {
-                    if(it.exists()){
+                    if (it.exists()) {
                         val password = it.child("password").value
-                        if(password!=passwordT){
-                            Toast.makeText(this
-                                ,"Incorrect Password"
-                                ,Toast.LENGTH_SHORT)
+                        if (password != passwordT) {
+                            Toast.makeText(
+                                this, "Incorrect Password", Toast.LENGTH_SHORT
+                            )
                                 .show()
-                        }
-                        else{
-                            Toast.makeText(this
-                            ,"Welcome ${it.child("name").value.toString()}"
-                            ,Toast.LENGTH_SHORT)
-                            .show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Welcome ${it.child("name").value.toString()}",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+
+                            val editor = sharedPreferences.edit()
+                            editor.apply {
+                                this.putString("username", usernameT)
+                                apply()
+                            }
+
                             val intent = Intent(this, homescreen::class.java)
                             startActivity(intent)
                             finish()
                         }
-                    }
-                    else{
-                        Toast.makeText(this
-                            ,"User doesn't exist"
-                            ,Toast.LENGTH_SHORT)
+                    } else {
+                        Toast.makeText(
+                            this, "User doesn't exist", Toast.LENGTH_SHORT
+                        )
                             .show()
                     }
                 }
-            }
-            else{
-                Toast.makeText(this
-                    ,"Please fill all the fields"
-                    ,Toast.LENGTH_SHORT)
+            } else {
+                Toast.makeText(
+                    this, "Please fill all the fields", Toast.LENGTH_SHORT
+                )
                     .show()
 
             }
