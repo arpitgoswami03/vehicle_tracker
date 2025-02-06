07a068df44930ed6618430dd0d6ed9db7e04629d
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-06 20:50:00 +0530

    no major change

--- app/src/main/java/com/example/vehicletracking/splashscreen.kt
+++ app/src/main/java/com/example/vehicletracking/splashscreen.kt
@@ -4,18 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.vehicletracking.databinding.ActivitySplashscreenBinding
 
 class splashscreen : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashscreenBinding
+    private lateinit var binding : ActivitySplashscreenBinding
     private var handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivitySplashscreenBinding.inflate(layoutInflater)
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         handler.postDelayed(
             {
