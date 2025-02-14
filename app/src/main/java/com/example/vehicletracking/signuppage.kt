1084a8b77606b8892b10c60015258b650248e1d3
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-14 18:59:15 +0530

    improved singup by using firebase authenticator

--- app/src/main/java/com/example/vehicletracking/signuppage.kt
+++ app/src/main/java/com/example/vehicletracking/signuppage.kt
@@ -3,21 +3,17 @@ package com.example.vehicletracking
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.vehicletracking.data_class.loginDetails
 import com.example.vehicletracking.databinding.ActivitySignuppageBinding
-import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class signuppage : AppCompatActivity() {
     private lateinit var binding: ActivitySignuppageBinding
     private var database = FirebaseDatabase.getInstance()
     private var reference = database.getReference("Users")
+    private var authenticator = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,52 +33,22 @@ class signuppage : AppCompatActivity() {
                     Toast.makeText(
                         this
                         ,"Password should be at least 8 characters"
-                        ,Toast.LENGTH_SHORT)
+                        ,Toast.LENGTH_SHORT
+                    )
                         .show()
                 }
-                else if(!emailT.contains("@gmail.com")){
+
+                if(!emailT.contains("@gmail.com")){
                     Toast.makeText(
                         this
                         ,"Please enter a valid email"
-                        ,Toast.LENGTH_SHORT)
+                        ,Toast.LENGTH_SHORT
+                    )
                         .show()
                 }
-                else {
-                    reference.child(usernameT).setValue(user).addOnSuccessListener {
-                        Toast.makeText(this, "Successfully registered", Toast.LENGTH_SHORT)
-                            .show()
-                        val intent = Intent(this, loginpage::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                        .addOnFailureListener {
-                            Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-//
-//                    checkUsernameUnique(usernameT) { isUnique ->
-//                        if (isUnique) {
-//                            reference.child(usernameT).setValue(user).addOnSuccessListener {
-//                                Toast.makeText(this, "Successfully registered", Toast.LENGTH_SHORT)
-//                                    .show()
-//                                val intent = Intent(this, loginpage::class.java)
-//                                startActivity(intent)
-//                                finish()
-//                            }
-//                            .addOnFailureListener {
-//                                Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT)
-//                                    .show()
-//                            }
-//                        } else {
-//                            Toast.makeText(
-//                                this,
-//                                "Username already exists. Please choose another.",
-//                                Toast.LENGTH_SHORT
-//                            )
-//                                .show()
-//                        }
-//                    }
-                }
+
+                createAccountWithFirebase(emailT,passwordT)
+                storeUserData(user)
 
             }
             else{
@@ -99,23 +65,43 @@ class signuppage : AppCompatActivity() {
         }
     }
 
-//    fun checkUsernameUnique(username: String, onResult: (Boolean) -> Unit) {
-//        val databaseReference = FirebaseDatabase.getInstance().getReference("users")
-//
-//        if (databaseReference != null) {
-//            databaseReference.child(username).addListenerForSingleValueEvent(object : ValueEventListener {
-//                override fun onDataChange(snapshot: DataSnapshot) {
-//                    onResult(!snapshot.exists()) // If it exists, it's not unique, so return false.
-//                }
-//
-//                override fun onCancelled(error: DatabaseError) {
-//                    println("Database error: ${error.message}")
-//                    onResult(false) // Treat cancellation as not unique for safety.
-//                }
-//            })
-//        } else {
-//            println("Database reference is null.")
-//            onResult(false)
-//        }
-//    }
+    private fun storeUserData(user: loginDetails) {
+        reference.child(user.username).setValue(user)
+            .addOnSuccessListener {
+                Toast.makeText(applicationContext
+                    ,"Successfully registered"
+                    ,Toast.LENGTH_SHORT
+                )
+                    .show()
+                val intent = Intent(applicationContext, loginpage::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(applicationContext
+                    ,"Failed to register"
+                    ,Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+    }
+
+    private fun createAccountWithFirebase(emailT: String, passwordT: String) {
+        authenticator.createUserWithEmailAndPassword(emailT,passwordT)
+            .addOnSuccessListener {
+                Toast.makeText(applicationContext
+                    ,"User created successfully"
+                    ,Toast.LENGTH_SHORT
+                )
+                    .show()
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext
+                    ,"Create account failed"
+                    ,Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+    }
+
 }
