c6cd2e4f7514d1dd6f19ba6ceb45eae487a80000
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-15 18:00:07 +0530

    removed checkbox and overrided onResume function

--- app/src/main/java/com/example/vehicletracking/loginpage.kt
+++ app/src/main/java/com/example/vehicletracking/loginpage.kt
@@ -2,27 +2,19 @@ package com.example.vehicletracking
 
 import android.content.Intent
 import android.content.SharedPreferences
-import android.content.SharedPreferences.Editor
 import android.os.Bundle
-import android.provider.Settings.Global.putString
-import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.vehicletracking.databinding.ActivityLoginpageBinding
-import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class loginpage : AppCompatActivity() {
     private lateinit var binding: ActivityLoginpageBinding
     private lateinit var sharedPreferences: SharedPreferences
     private val database = FirebaseDatabase.getInstance()
     private val reference = database.getReference("Users")
+    private val authenticator = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,50 +27,48 @@ class loginpage : AppCompatActivity() {
             startActivity(intent)
         }
         binding.loginButton.setOnClickListener {
-            val usernameT = binding.username.text.toString()
+            val emailT = binding.email.text.toString()
             val passwordT = binding.password.text.toString()
-            if (usernameT.isNotEmpty() && passwordT.isNotEmpty()) {
-                Log.i("firebase", "All fields are filled")
-                reference.child(usernameT).get().addOnSuccessListener {
-                    if (it.exists()) {
-                        val password = it.child("password").value
-                        if (password != passwordT) {
-                            Toast.makeText(
-                                this, "Incorrect Password", Toast.LENGTH_SHORT
-                            )
-                                .show()
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "Welcome ${it.child("name").value.toString()}",
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
 
-                            val editor = sharedPreferences.edit()
-                            editor.apply {
-                                this.putString("username", usernameT)
-                                apply()
-                            }
-
-                            val intent = Intent(this, homescreen::class.java)
-                            startActivity(intent)
-                            finish()
-                        }
-                    } else {
-                        Toast.makeText(
-                            this, "User doesn't exist", Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    }
-                }
-            } else {
+            if (emailT.isNotEmpty() && passwordT.isNotEmpty()) {
+                loginWithFirebase(emailT, passwordT)
+            }
+            else {
                 Toast.makeText(
-                    this, "Please fill all the fields", Toast.LENGTH_SHORT
+                    this
+                    , "Please fill all the fields"
+                    , Toast.LENGTH_SHORT
                 )
                     .show()
 
             }
         }
     }
+
+    private fun loginWithFirebase(emailT: String, passwordT: String) {
+        authenticator.signInWithEmailAndPassword(emailT,passwordT)
+            .addOnSuccessListener {
+                Toast.makeText(this
+                    ,"Login Successful"
+                    ,Toast.LENGTH_SHORT
+                ).show()
+                startActivity(Intent(applicationContext,homescreen::class.java))
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this
+                    ,"Login Failed"
+                    ,Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        authenticator.currentUser?.apply {
+                startActivity(Intent(applicationContext, homescreen::class.java))
+                finish()
+        }
+    }
+
 }
