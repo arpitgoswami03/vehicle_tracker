301c67193fa4727e0648d53d21777806ebc5e3e8
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-22 22:06:49 +0530

    improved functionality

--- app/src/main/java/com/example/vehicletracking/homescreen.kt
+++ app/src/main/java/com/example/vehicletracking/homescreen.kt
@@ -1,21 +1,26 @@
 package com.example.vehicletracking
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.vehicletracking.databinding.ActivityHomescreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class homescreen : AppCompatActivity() {
-    private lateinit var binding: ActivityHomescreenBinding
+    private lateinit var binding : ActivityHomescreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.title = "Vehicle Tracker"
         binding = ActivityHomescreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setFragment(garageFragment())
+
+        val currentUser = FirebaseAuth.getInstance().currentUser?.email.toString()
+        Log.i("currentUser", currentUser)
         binding.homenavbar.selectedItemId = R.id.garage
         binding.homenavbar.setOnItemSelectedListener {
             when (it.itemId) {
