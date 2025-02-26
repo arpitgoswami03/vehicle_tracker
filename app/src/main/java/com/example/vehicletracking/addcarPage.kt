bba5be7fa0e4a4fb9bb884407a646c84afb8d543
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-26 15:37:45 +0530

    added cancel btn and successful data addition to storage

--- app/src/main/java/com/example/vehicletracking/addcarPage.kt
+++ app/src/main/java/com/example/vehicletracking/addcarPage.kt
@@ -1,29 +1,18 @@
 package com.example.vehicletracking
 
-import android.Manifest
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_MEDIA_IMAGES
-import android.app.Activity.RESULT_OK
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
 import com.example.vehicletracking.data_class.carDetails
-import com.example.vehicletracking.databinding.FragmentAddcarBinding
+import com.example.vehicletracking.databinding.ActivityAddcarBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -33,15 +22,14 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
-class addcarFragment : Fragment() {
-    private lateinit var binding: FragmentAddcarBinding
+class addcarPage : AppCompatActivity() {
+    private lateinit var binding: ActivityAddcarBinding
     private val READ_STORAGE_CODE = 100
     private var imageUri: Uri? = null
-    private var firebaseDB: FirebaseDatabase = FirebaseDatabase.getInstance()
-    private var firebaseRef: DatabaseReference = firebaseDB.getReference("Users")
-    private var firebaseStorage: FirebaseStorage = FirebaseStorage.getInstance()
-    private var storageRef: StorageReference = firebaseStorage.getReference("carimage")
-    private val authenticator = FirebaseAuth.getInstance()
+    private var firebaseDB: DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
+    private var firebaseStorage: StorageReference =
+        FirebaseStorage.getInstance().getReference("carimage")
+    private val firebaseAuthenticator = FirebaseAuth.getInstance()
 
     // Take image from gallery (2)
     private val startActivityForResult =
@@ -64,26 +52,24 @@ class addcarFragment : Fragment() {
 
 
     // Main function
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        binding = FragmentAddcarBinding.inflate(layoutInflater)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddcarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Fill the details about car and handle permission (1)
         binding.addImg.setOnClickListener {
-                if (ContextCompat.checkSelfPermission(requireContext(), READ_MEDIA_IMAGES)
-                    != PackageManager.PERMISSION_GRANTED
-                    ) {
-                    ActivityCompat.requestPermissions(requireActivity(),arrayOf(READ_MEDIA_IMAGES), READ_STORAGE_CODE)
-                }
-                else {
-                    launchGallery()
-                }
-        }
-
-        binding.closeBtn.setOnClickListener {
-            setFragment(garageFragment())
+            if (ContextCompat.checkSelfPermission(this, READ_MEDIA_IMAGES)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(READ_MEDIA_IMAGES),
+                    READ_STORAGE_CODE
+                )
+            } else {
+                launchGallery()
+            }
         }
 
         // Upload image and save details (3)
@@ -99,14 +85,20 @@ class addcarFragment : Fragment() {
                 && numberT.isNotEmpty()
             ) {
                 uploadImageAndSaveDetails(nameT, modelT, numberT, imageT!!)
+                finish()
             } else {
                 Toast.makeText(
-                    requireContext(), "Please fill all the Details", Toast.LENGTH_SHORT
+                    this, "Please fill all the Details", Toast.LENGTH_SHORT
                 ).show()
                 Log.e("UploadProcess", "Text field might be empty")
             }
         }
-        return binding.root
+
+        binding.cancelBtn.setOnClickListener {
+            val intent = Intent(this, homescreen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     // Find reference to the firestorage (4)
@@ -117,7 +109,7 @@ class addcarFragment : Fragment() {
         imageUri: Uri,
     ) {
 
-        storageRef.child(number).putFile(imageUri)
+        firebaseStorage.child(number).putFile(imageUri)
             .addOnSuccessListener { task ->
                 Log.d("storageImg", "Image upload successful.")
                 task.metadata!!.reference!!.downloadUrl
@@ -138,19 +130,18 @@ class addcarFragment : Fragment() {
 
     // Upload the data to the database (5)
     private fun saveCarDetails(name: String, model: String, number: String, imageUrl: String) {
-        val userEmail = authenticator.currentUser?.email.toString()
+        val userEmail = firebaseAuthenticator.currentUser?.email.toString()
 
         if (userEmail.isNotEmpty()) {
             getUsername(userEmail) { username ->
                 if (username != null) {
                     val car = carDetails(name, model, number, imageUrl)
-                    firebaseRef.child(username)
+                    firebaseDB.child(username)
                         .child("Car Available")
                         .child("$name $model")
                         .setValue(car)
                         .addOnSuccessListener {
                             Log.i("firebase", "Car details saved successfully")
-                            setFragment(garageFragment())
                         }
                         .addOnFailureListener { exception ->
                             Log.e("firebase", "Failed to save car details: ${exception.message}")
@@ -164,13 +155,6 @@ class addcarFragment : Fragment() {
         }
     }
 
-
-    private fun setFragment(fragment: Fragment){
-        parentFragmentManager.beginTransaction()
-            .replace(R.id.homefrag,fragment)
-            .commit()
-    }
-
     private fun launchGallery() {
         val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
             type = "image/*"
@@ -179,7 +163,7 @@ class addcarFragment : Fragment() {
     }
 
     private fun getUsername(currentUserEmail: String, callback: (String?) -> Unit) {
-        firebaseRef.orderByChild("email").equalTo(currentUserEmail)
+        firebaseDB.orderByChild("email").equalTo(currentUserEmail)
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     val username = snapshot.children.firstOrNull()
@@ -195,7 +179,6 @@ class addcarFragment : Fragment() {
                 }
             })
     }
-
 
 
 }
