5c3479744bf7de9e110face3d7e523113ca3a3ae
arpitgoswami03 <arpit.personal.2003@gmail.com>
2025-02-27 12:46:18 +0530

    changed fragment to activity

--- app/src/main/java/com/example/vehicletracking/mapspage.kt
+++ app/src/main/java/com/example/vehicletracking/mapspage.kt
@@ -1,4 +1,17 @@
 package com.example.vehicletracking
 
-class mapspage {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.vehicletracking.databinding.ActivityMappageBinding
+
+class mapspage: AppCompatActivity() {
+    private lateinit var binding: ActivityMappageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMappageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
 }
